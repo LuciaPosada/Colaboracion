17bdb0e96e0b0cb3d162a544a548797c4c07c0f0
ssusibel <dam1@aulaAulainformatica1>
2023-10-20 09:51:54 +0200

    B

---
+++ src/Colaborador.java
@@ -0,0 +1,12 @@
+public class Colaborador {
+    public static void main(String[] args) {
+        //Introducimos datos
+        float base=4, altura=3, area;
+
+        //Calculamos
+        area=base*altura/2;
+
+        //Mostramos
+        System.out.println("area="+area);
+    }
+}
